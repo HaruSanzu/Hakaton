701839bfeb8b5edf90b02146a2035803dbb08dbf
HaruSanzu <115856831+HaruSanzu@users.noreply.github.com>
2023-10-29 16:20:57 +0600

    Update telega_main.py

--- telega_main.py
+++ telega_main.py
@@ -20,8 +20,8 @@ def start(message):
 def on_click(message):
     if message.text == 'Начать заполнение':
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        btn2 = types.KeyboardButton('Группа 1 ')
-        btn3 = types.KeyboardButton('Группа 2 ')
+        btn2 = types.KeyboardButton('Группа 1')
+        btn3 = types.KeyboardButton('Группа 2')
         markup.add(btn2)
         markup.add(btn3)
         bot.send_message(message.chat.id, 'Выберите группу', reply_markup=markup)
@@ -70,9 +70,8 @@ def student(message):
     if message.text == 'Adil'  or message.text == 'Kuka' or message.text == 'Nurik' or message.text == 'Nurken':
         btn8 = types.KeyboardButton('Домашка')
         btn9 = types.KeyboardButton('Классная Работа')
-        btn10 = types.KeyboardButton('Контрольная Работа')
         btn11 = types.KeyboardButton('Назад')
-        markup.add(btn8,btn9,btn10)
+        markup.add(btn8,btn9)
         markup.add(btn11)
         bot.send_message(message.chat.id,"Выберите Действие",reply_markup=markup)
     else:
@@ -119,6 +118,15 @@ def ask_ex_count(message):
     bot.send_message(message.chat.id, 'Какие задачи были выполнены?')
     bot.register_next_step_handler(message, save_homework_ex_count)
 
+def save_homework_ex_count(message):
+    connection = sqlite3.connect('C:\\Users\\user\\IdeaProjects\\projectX\\database')
+    cursor = connection.cursor()
+    student_id = 1
+    homework = message.text
+    cursor.execute("UPDATE students SET homework = ? WHERE student_id = 1" ,(homework,))
+    connection.commit()
+    bot.send_message(message.chat.id, 'Задачи сохранены')
+
 @bot.message_handler(func=lambda message: message.text == 'Назад')
 def go_back(message):
     group = selected_group.get(message.chat.id)
@@ -138,12 +146,13 @@ def go_back(message):
         markup.add(btn_back)
         bot.send_message(message.chat.id, 'Выберите Ученика', reply_markup=markup)
 
+
 @bot.message_handler(func=lambda message: message.text=='Классная Работа')
 def ask_theme(message):
     bot.send_message(message.chat.id, 'Напишите тему')
-    bot.register_next_step_handler(message, save_homework_topic)
+    bot.register_next_step_handler(message, save_classwork_topic)
 
-def save_homework_topic(message):
+def save_classwork_topic(message):
     connection = sqlite3.connect('C:\\Users\\user\\IdeaProjects\\projectX\\database')
     cursor = connection.cursor()
     student_id = 1
@@ -154,7 +163,17 @@ def save_homework_topic(message):
 @bot.message_handler(func=lambda message: message.text == 'Далее')
 def ask_ex_count(message):
     bot.send_message(message.chat.id, 'Какие задачи были выполнены?')
-    bot.register_next_step_handler(message, save_homework_ex_count)
+    bot.register_next_step_handler(message, save_classwork_ex_count)
+
+def save_classwork_ex_count(message):
+    connection = sqlite3.connect('C:\\Users\\user\\IdeaProjects\\projectX\\database')
+    cursor = connection.cursor()
+    student_id = 1
+    classwork = message.text
+    cursor.execute("UPDATE students SET classwork = ? WHERE student_id = 1" ,(classwork,))
+    connection.commit()
+    bot.send_message(message.chat.id, 'Задачи сохранены')
+
 
 @bot.message_handler(func=lambda message: message.text == 'Назад')
 def go_back(message):
@@ -174,25 +193,6 @@ def go_back(message):
         markup.add(btn6, btn7)
         markup.add(btn_back)
         bot.send_message(message.chat.id, 'Выберите Ученика', reply_markup=markup)
-
-def save_homework_ex_count(message):
-    connection = sqlite3.connect('C:\\Users\\user\\IdeaProjects\\projectX\\database')
-    cursor = connection.cursor()
-    student_id = 1
-    classwork = message.text
-    cursor.execute("UPDATE students SET classwork = ? WHERE student_id = 1" ,(classwork,))
-    connection.commit()
-    bot.send_message(message.chat.id, 'Задачи сохранены')
-
-
-
-
-
-
-
-
-
-
 
 
 @bot.message_handler(commands=['students'])
@@ -204,7 +204,7 @@ def students(message):
     connection.close()
     message_text = ''
     for student in students:
-        message_text += f'{student[1]} - {student[2]}\n'
+        message_text += f'{student[1]} - {student[2]} - {student[3]} - {student[4]} - {student[6]} - {student[7]}\n'
     bot.send_message(message.chat.id, message_text)
 
 
